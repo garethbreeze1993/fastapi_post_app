ecc52dbf682d43c86bec31862562a8dce0c2d75d
Gareth Breeze <gareth.breeze@hypaship.com>
2022-01-31 19:22:03 +0000

    Change root message and successfuly finish tutorial

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my application check deploy to ubuntu"}
+    return {"message": "Welcome to my application!!!"}
